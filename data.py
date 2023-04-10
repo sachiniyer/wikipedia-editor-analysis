0668370d96d4f37675af96c5553532f04ab123ff
Sachin Iyer <sachinjiyer@gmail.com>
2023-04-10 12:36:34 -0400

    clean script a bit

--- data.py
+++ data.py
@@ -1,43 +1,33 @@
 #!/usr/bin/env python3
 import requests
 
-# set up the API endpoint URL
 url = "https://en.wikipedia.org/w/api.php"
 
-# set the parameters for the API request
 params = {
     "action": "query",
     "list": "allpages",
-    "aplimit": 10,  # number of pages to retrieve in each API call
+    "aplimit": 10,
     "format": "json"
 }
 
-# set up a session object to handle cookies and headers
 session = requests.Session()
 
-# loop over each batch of pages and get the list of contributors for each page
 while True:
-    # make the API request and get the response data as JSON
+
     response = session.get(url, params=params)
     data = response.json()
 
-    # loop over each page and get the contributors
     for page in data["query"]["allpages"]:
         page_title = page["title"]
 
-        # set the parameters for the contributors API request
-        contributors_params = {
             "action": "query",
             "prop": "contributors",
             "titles": page_title,
             "format": "json"
         }
 
-        # make the API request and get the response data as JSON
         contributors_response = session.get(url, params=contributors_params)
         contributors_data = contributors_response.json()
-
-        # set the parameters for the data API request
 
         data_params = {
             "action": "query",
@@ -50,8 +40,7 @@ while True:
 
         data_response = session.get(url, params=data_params)
         data_data = data_response.json()
-        print(data_data)
-        # extract the contributor information and print it to the console
+
         pages = contributors_data["query"]["pages"]
         for page_id in pages:
             page = pages[page_id]
